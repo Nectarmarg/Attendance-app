d7eaf6e67c152fa551ed6ae51978362437d3219b
Nektarios <nmaragkos1@gmail.com>
2025-02-28 00:18:16 +0200

    added basic layout of attendance page

--- attendance.php
+++ attendance.php
@@ -12,12 +12,33 @@ if (!isset($_SESSION['current_user'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/attendance.css">
     <title>Document</title>
 </head>
 
 <body>
-    <h1>Hello</h1>
-    <button id="btnLogοut">LOGOUT</button>
+    <div class="page">
+        <div class="header-area">
+            <div class="logo-area">
+                <h1 class="logo">ATTENDANCE APP</h1>
+            </div>
+            <div class="logout-area"><button class="logout">LOGOUT</button></div>
+        </div>
+        <div class="session-area">
+            <div class="label-area"><label for="">SESSION</label></div>
+            <div class="dropdown-area">
+                <select name="" id="" class="ddlclass">
+                    <option value="">SELECT ONE</option>
+                    <option value="">2023 AUTUMN</option>
+                    <option value="">2023 SPRING</option>
+                </select>
+            </div>
+        </div>
+        <div class="classlist-area">1</div>
+        <div class="classdetails-area">1</div>
+        <div class="studentlist-area">1</div>
+    </div>
+
     <script src="js/jquery.js"></script>
     <script src="js/logout.js"></script>
 </body>
